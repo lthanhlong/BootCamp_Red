854c9b407aadc487325ba42bb07c2661c5f0d979
unknown <trong.phamtranduc@gmail.com>
2021-02-03 01:02:26 +0700

    Navbar + login + logout

--- FlappyBird/src/app/app.component.ts
+++ FlappyBird/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FlappyBird';
-}
+
+  constructor(private auth:AngularFireAuth){}
+ async logIn(){
+      const provider = new firebase.default.auth.GoogleAuthProvider();
+      try{
+        await this.auth.signInWithPopup(provider);
+        alert("login successfully");
+      }catch(err){
+        alert("failed");
+      }
+
+    }
+  async logOut(){
+    try{
+      await this.auth.signOut();
+      alert("Sigout successfully")
+    }catch(err){
+      alert("Sigout failed")
+    }
+  }
+  }
+
