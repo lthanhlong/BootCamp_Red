208828d55902552ea980fa6b24d6b6606884556a
Le Thanh Long <long.lt1212@sinhvien.hoasen.edu.vn>
2021-02-03 10:05:10 +0700

    Home

--- FlappyBird/src/app/app.component.spec.ts
+++ FlappyBird/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FlappyBird'`, () => {
+  it(`should have as title 'Project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FlappyBird');
+    expect(app.title).toEqual('Project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FlappyBird app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Project app is running!');
   });
 });
