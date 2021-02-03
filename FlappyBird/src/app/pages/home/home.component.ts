906dc903518a9f311f7255478f2997a76be6a492
Le Thanh Long <long.lt1212@sinhvien.hoasen.edu.vn>
2021-02-03 17:14:34 +0700

    check lan 4

--- FlappyBird/src/app/pages/home/home.component.ts
+++ FlappyBird/src/app/pages/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, query, style, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('animImageSlider', [
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
-
+  counter: number = 0;
+  images = [
+    'https://placeimg.com/300/300/nature/6',
+    'https://placeimg.com/300/300/nature/7',
+    'https://placeimg.com/300/300/nature/8',
+    'https://placeimg.com/300/300/nature/9',
+    'https://placeimg.com/300/300/nature/2',
+    'https://placeimg.com/300/300/nature/3',
+    'https://placeimg.com/300/300/nature/1',
+  ];
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onNext() {
+    if (this.counter != this.images.length - 1) {
+      this.counter++;
+    }
+  }
+
+  onPrevious() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
   }
 
 }
